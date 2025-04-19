770ca27940b1a30eec60ec2816e5273eca21e2bf
mimikousi <mimikousi0@gmail.com>
2025-04-19 12:12:41 +0900

    remove_analytics

--- pages/book.py
+++ pages/book.py
@@ -3,7 +3,6 @@
 import streamlit as st
 from aozora import get_aozora, parse_text_into_sentences
 from wc import get_tokens, get_pos_ids, get_wordcloud
-from sentiment import get_model, get_sentiment, SENTIMENTS
 
 @st.cache_data
 def retrieve_aozora(url):
@@ -23,13 +22,7 @@ def process_tokens(text):
     return get_tokens(text)
 
 
-@st.cache_resource
-def prepare_sentiment_model():
-    print(f'Preparing the model.')
-    return get_model()
-
-
-tab_text, tab_wc, tab_sentiment = st.tabs(['テキスト', 'ワードクラウド', '感情分析'])
+tab_text, tab_wc = st.tabs(['テキスト', 'ワードクラウド'])
 
 with tab_text:
     aozora_url = st.text_input('**青空文庫 Zip ファイル URL**', value='https://www.aozora.gr.jp/cards/000074/files/427_ruby_19792.zip')
@@ -37,13 +30,6 @@ with tab_text:
 with tab_wc:
     pos_ids = prepare_pos_ids()
     pos_list = st.multiselect('品詞', pos_ids, default='名詞')
-
-with tab_sentiment:
-    trace_on = st.checkbox('文単位の感情', value=False)
-    graph = st.empty()
-    with graph:
-        bar = st.progress(value=0.0, text='計算中')
-
 
 if aozora_url is not None:
     try:
@@ -62,18 +48,3 @@ if aozora_url is not None:
         tokens = process_tokens(text)
         img = get_wordcloud(tokens, pos_list)
         st.image(img, width=800)
-
-    with tab_sentiment:
-        pipe = prepare_sentiment_model()
-        sentences = parse_text_into_sentences(text)
-        sentiments = {key: 0 for key in SENTIMENTS}
-
-        for idx, sentence in enumerate(sentences):
-            emotion = get_sentiment(pipe, sentence);
-            bar.progress(value=idx/len(sentences), text='計算中')
-            sentiments[emotion] = sentiments[emotion] + 1
-
-            if trace_on is True:
-                st.markdown(f'{sentence} ==> {emotion}')
-
-        graph.bar_chart(sentiments)
